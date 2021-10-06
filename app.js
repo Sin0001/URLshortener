979ff17651511f5b703d571ac3fdfb3f0016405e
Sin0001 <bboyliljie@gmail.com>
2021-10-06 18:51:58 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost/URLshortener')
+
+const db = mongoose.connection
+
+db.on('erorr', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req,res) => {
   res.send(`it will be a URLshortener`)
